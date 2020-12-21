587dabdb21d2f33875c6d66162ee9527bb7795c1
Evgeniy Klimov <vikkes@yandex.ru>
2020-12-22 00:49:29 +0700

    add Serilog Middleware

--- ImageToPuzzle/Program.cs
+++ ImageToPuzzle/Program.cs
@@ -1,3 +1,4 @@
+using ImageToPuzzle.Middleware;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -10,11 +11,10 @@ namespace ImageToPuzzle
 			CreateHostBuilder(args).Build().Run();
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+		private static IHostBuilder CreateHostBuilder(string[] args)
+		{
+			return Host.CreateDefaultBuilder(args)
+				.UseWebPartstatCoreBuilder<Startup>();
+		}
 	}
 }
